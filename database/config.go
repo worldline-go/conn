8c184eb68c79a4d88e0eae60ee4c9fc7cd96c3f9
Eray Ates <eray.ates@worldline.com>
2025-09-05 15:33:04 +0200

    fix: update db conn
    Signed-off-by: Eray Ates <eray.ates@worldline.com>

--- database/config.go
+++ database/config.go
@@ -9,4 +9,13 @@ type Config struct {
 	ConnMaxLifetime time.Duration `cfg:"conn_max_lifetime" default:"15m"`
 	MaxIdleConns    int           `cfg:"max_idle_conns"    default:"3"`
 	MaxOpenConns    int           `cfg:"max_open_conns"    default:"3"`
+
+	Migration Migration `cfg:"migration"`
+}
+
+type Migration struct {
+	Datasource string `cfg:"datasource" log:"-"`
+	Type       string `cfg:"type"`
+	Schema     string `cfg:"schema"`
+	Table      string `cfg:"table"`
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.24
 require (
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/jmoiron/sqlx v1.4.0
-	github.com/redis/go-redis/v9 v9.10.0
+	github.com/redis/go-redis/v9 v9.13.0
 	github.com/twmb/tlscfg v1.2.1
 )
 
